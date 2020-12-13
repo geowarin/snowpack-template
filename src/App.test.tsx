9a44322c98104d297d24de2dd37d00a84f307fed
geowarin <code@geowarin.com>
2020-12-13 13:16:35 +0100

    Jest setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,11 @@
 import { h } from 'preact';
 import { render } from '@testing-library/preact';
-import { expect } from 'chai';
 import App from './App';
 
 describe('<App>', () => {
   it('renders learn react link', () => {
     const { getByText } = render(<App />);
     const linkElement = getByText(/learn preact/i);
-    expect(document.body.contains(linkElement));
+    expect(linkElement).toBeInTheDocument();
   });
 });
